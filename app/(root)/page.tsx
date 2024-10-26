f49bde0b8da153a489280a05624404cfbdaf5d22
Anna N Helms <unicornbuddha@gmail.com>
2024-10-26 12:48:29 -0700

    Home page set up: Sidebar, RightSidebar, MobileNav, and BankCard. Adds shadcn ui component sheet

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,28 +1,28 @@
 import HeaderBox from "@/components/HeaderBox";
+import RightSidebar from "@/components/RightSidebar";
 import TotalBalanceBox from "@/components/TotalBalanceBox";
 import React from "react";
 
 const Home = () => {
-  const loggedIn = { firstName: 'Adrian' };
+  const loggedIn = { firstName: "Adrian", lastName: "JSM", email: "contact@jsmastery.pro" };
 
   return (
     <section className="home">
       <div className="home-content">
         <header className="home-header">
-          <HeaderBox 
+          <HeaderBox
             type="greeting"
             title="Welcome"
-            user={loggedIn?.firstName || 'Guest'}
+            user={loggedIn?.firstName || "Guest"}
             subtext="Access and manage your account and transactions efficiently."
           />
 
-          <TotalBalanceBox 
-            accounts={[]}
-            totalBanks={1}
-            totalCurrentBalance={1250.35}
-          />
+          <TotalBalanceBox accounts={[]} totalBanks={1} totalCurrentBalance={1250.35} />
         </header>
+        RECENT TRANSATIONS
       </div>
+
+      <RightSidebar user={loggedIn} transactions={[]} banks={[{ currentBalance: 123.5 }, { currentBalance: 500.5 }]} />
     </section>
   );
 };
