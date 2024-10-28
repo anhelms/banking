0410f032fe21a467a3bf79727179ab7cc63483b6
Anna N Helms <unicornbuddha@gmail.com>
2024-10-28 14:42:07 -0700

    Creates AuthForm, SignIn, SignUp, CustomInput, and adds shadcn form, label, input to lib

---
+++ lib/actions/user.actions.ts
@@ -0,0 +1,55 @@
+"use server";
+
+import { cookies } from "next/headers";
+import { ID } from "node-appwrite";
+import { createAdminClient, createSessionClient } from "../appwrite";
+import { parseStringify } from "../utils";
+
+export const signIn = async () => {
+  try {
+    // Mutation / Database / Make fetch
+  } catch (error) {
+    console.error("Error", error);
+  }
+};
+
+export const signUp = async ({  password, ...userData }: SignUpParams) => {
+  const { email, firstName, lastName } = userData;
+
+  let newUserAccount;
+
+  try {
+    const { account, database } = await createAdminClient();
+
+    newUserAccount = await account.create(
+      ID.unique(), 
+      email, 
+      password, 
+      `${firstName} ${lastName}`
+    );
+
+    const session = await account.createEmailPasswordSession(email, password);
+
+    cookies().set("appwrite-session", session.secret, {
+      path: "/",
+      httpOnly: true,
+      sameSite: "strict",
+      secure: true,
+    });
+
+    return parseStringify(newUserAccount);
+  } catch (error) {
+    console.error("Error", error);
+  }
+};
+
+// ... your initilization functions
+
+export async function getLoggedInUser() {
+  try {
+    const { account } = await createSessionClient();
+    return await account.get();
+  } catch (error) {
+    return null;
+  }
+}
